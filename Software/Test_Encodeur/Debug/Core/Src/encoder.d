9b17b5a1f6a5f3920e865e8618569cfabcec9922
aymebenj27 <aymen.benjemaa@ensea.fr>
2025-12-02 12:43:55 +0100

    enc_mot_drv

--- Software/Test_Encodeur/Debug/Core/Src/encoder.d
+++ Software/Test_Encodeur/Debug/Core/Src/encoder.d
@@ -1,6 +1,5 @@
-Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.o: \
- ../Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c \
- ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
+Core/Src/encoder.o: ../Core/Src/encoder.c ../Core/Inc/encoder.h \
+ ../Core/Inc/main.h ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
  ../Core/Inc/stm32g4xx_hal_conf.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h \
@@ -27,6 +26,8 @@ Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.o: \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h
+../Core/Inc/encoder.h:
+../Core/Inc/main.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
 ../Core/Inc/stm32g4xx_hal_conf.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h:
--- Software/Test_Encodeur/Debug/Core/Src/motor.d
+++ Software/Test_Encodeur/Debug/Core/Src/motor.d
@@ -1,6 +1,5 @@
-Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.o: \
- ../Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.c \
- ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
+Core/Src/motor.o: ../Core/Src/motor.c ../Core/Inc/motor.h \
+ ../Core/Inc/main.h ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h \
  ../Core/Inc/stm32g4xx_hal_conf.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_def.h \
@@ -27,6 +26,8 @@ Drivers/STM32G4xx_HAL_Driver/Src/stm32g4xx_hal_gpio.o: \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_pwr_ex.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim.h \
  ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_tim_ex.h
+../Core/Inc/motor.h:
+../Core/Inc/main.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal.h:
 ../Core/Inc/stm32g4xx_hal_conf.h:
 ../Drivers/STM32G4xx_HAL_Driver/Inc/stm32g4xx_hal_rcc.h:
